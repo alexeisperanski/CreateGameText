495949b41bf5019fa03b53b59a4d5fdafb3eb603
alexeisperanski <39588160+alexeisperanski@users.noreply.github.com>
2018-12-08 20:01:23 +0300

    Create New File

--- CreateGame.py
+++ CreateGame.py
@@ -1,2 +1,21 @@
+import string
+import math
+import fileinput
+import os
+
 def createf(fileName):
-    
+    fileS = os.listdir(path=".")
+    print(fileS)
+    for s in fileS:
+        if fileName == s:
+            return False
+    with open(fileName, "w"):
+        print("Файл создан")
+    return True
+
+createf("first")
+createf("second")
+createf("first")
+
+
+
