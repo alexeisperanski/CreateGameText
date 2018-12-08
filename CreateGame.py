63a4750fe9417e520c7b99f0dffdd33409cc0a6b
alexeisperanski <39588160+alexeisperanski@users.noreply.github.com>
2018-12-09 00:47:39 +0300

    Фикс багов

--- CreateGame.py
+++ CreateGame.py
@@ -1,25 +1,20 @@
-import string
-import math
-import fileinput
 import os
 global allFileName
 global fileformat
 fileformat = ".txt"
-allFileName = []
 allFileName = ['start.txt']
 
-def createf(fileName):
+def createf(fileName):  #создание файла
     fileS = os.listdir(path=".")
-    print(fileS)
     for s in fileS:
         if fileName == s:
             print("Файл с данным именем уже существует")
             return False
     with open(fileName, "w"):
-        print("Файл создан")
+        print(f"Файл {fileName} создан")
     return True
 
-def enterMission(fileName):
+def enterMission(fileName): #введение информация о ступени для чтения
     with open(fileName, "a") as file:
         file.write(input("Введите основной текст миссии: ") + "\n")
         otv = int(input("Введите сколько будет вариантов ответов: "))
@@ -27,7 +22,7 @@ def enterMission(fileName):
         for value in range(otv):
             file.write(input(f"Вариант ответа №{value + 1}: ") + "\n")
             file.write(input(f"Вариант реакции №{value + 1}: ") + "\n")
-            link = input(f"Имя нового файла для перехода с ответ №{value + 1}: ")
+            link = input(f"Имя нового файла для перехода с ответ №{value + 1}: ") + fileformat
             file.write(f"{link}\n")
             if link in allFileName:
                 print("Ссылка на уже готовый файл")
@@ -38,19 +33,22 @@ print("Вы создёте первый шаг для игры\n")
 Newfileform = input("Введите формат ваших файлов для записи(пустой, если .txt): ")
 if Newfileform != "":
     fileformat = Newfileform
-missionName = input("Введите имя нового файла")
-if createf(missionName):
-    enterMission(missionName)
+
 Tr = True
 while Tr:
-    number = input(allFileName, "\nВыберите файл, который будете создавать по номеру позиции\nИли нажмите Enter чтобы закончить: ")
+    if allFileName == []:
+        Tr = False
+        print("Все файлы уже созданы, программа имеет конец")
+        continue
+    print(f"\n\nЕщё не заполненные файлы: \n{allFileName[:]}")
+    number = input("\nВыберите файл, который будете заполнять по номеру позиции\nИли нажмите Enter чтобы закончить: ")
     if number == "":
+        print(f"Вы закончили запонение, но ваша игра имеет ссылки на несозданные файлы: {allFileName[:]}")
         Tr = False
         continue
-    missionName = allFileName.pop(number - 1)
+    missionName = allFileName.pop(int(number) - 1)
     if createf(missionName):
         enterMission(missionName)
-
 
 
 
