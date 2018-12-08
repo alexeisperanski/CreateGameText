34827233a8c1852c6f5f116249661fd830eda068
alexeisperanski <39588160+alexeisperanski@users.noreply.github.com>
2018-12-08 21:02:11 +0300

    Create all mission

--- CreateGame.py
+++ CreateGame.py
@@ -3,6 +3,8 @@ import math
 import fileinput
 import os
 global allFileName
+global fileformat
+fileformat = ".txt"
 allFileName = []
 
 def createf(fileName):
@@ -22,14 +24,35 @@ def enterMission(fileName):
         otv = int(input("Введите сколько будет вариантов ответов: "))
         file.write(f"{otv}\n")
         for value in range(otv):
-            file.write(input(f"Вариант ответа №{value}: ") + "\n")
-            file.write(input(f"Вариант реакции №{value}: ") + "\n")
-            link = input(f"Имя нового файла для перехода с ответ №{value}: ")
+            file.write(input(f"Вариант ответа №{value + 1}: ") + "\n")
+            file.write(input(f"Вариант реакции №{value + 1}: ") + "\n")
+            link = input(f"Имя нового файла для перехода с ответ №{value + 1}: ")
             file.write(f"{link}\n")
             if link in allFileName:
                 print("Ссылка на уже готовый файл")
             else:
                 allFileName.append(link)
+
+print("Вы создёте первый шаг для игры\n")
+Newfileform = input("Введите формат ваших файлов для записи(пустой, если .txt): ")
+if Newfileform != "":
+    fileformat = Newfileform
+missionName = input("Введите имя нового файла")
+if createf(missionName):
+    enterMission(missionName)
+Tr = True
+while Tr:
+    number = input(allFileName, "\nВыберите файл, который будете создавать по номеру позиции\nИли нажмите Enter чтобы закончить: ")
+    if number == "":
+        Tr = False
+        continue
+    missionName = allFileName.pop(number - 1)
+    if createf(missionName):
+        enterMission(missionName)
+
+
+
+
 
 
 
